1659a7d266c8a6da89a2dacb2414649f0be6901b
hungtrd <hungtranduy.dev@gmail.com>
2025-11-21 11:49:53 +0700

    update package

---
+++ base62/rand.go
@@ -0,0 +1,9 @@
+package base62
+
+func RandStr(length int) string {
+	return ""
+}
+
+func RandNumeric(length int) string {
+	return ""
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/hungtrd/gopkg
 
-go 1.22.3
+go 1.25.1
